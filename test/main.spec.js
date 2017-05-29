23691dc9e0371c8b672837a368ff1ab44e420a60
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-05-30 01:07:08 +0800

    add spec for pipe function

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,16 @@
+const { pipe, branchIf, selector, selectorPipe } = require('../index')
+
 describe('pipe function', () => {
-  it('will return state correctly', () => {
-    expect(1).toBe(1)
+  it('will return a new state that is applied with mutations', () => {
+    const state = 0
+    const addOne = state => state + 1
+    const addTwo = state => state + 2
+    const addThree = state => state + 3
+    expect(pipe([addOne, addTwo, addThree], state)).toBe(6)
+  })
+  it('will throw an error if firstParam is not array', () => {
+    expect(() => pipe(1)).toThrow(
+      new Error('First parameter of pipe must be an array')
+    )
   })
 })
