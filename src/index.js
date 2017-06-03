3808f27d1b9a9a1e83a279099e7b08fdf89aa453
Jonathan Phoon <jonathan.pwh@gmail.com>
2017-06-04 02:43:37 +0800

    change export to module.export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { isImmutable } from 'immutable'
 import { isFirstParamArray, reduceState, areAllFunctions } from './helpers.js'
 
-export const pipe = (arrayOfMutators, state) => {
+const pipe = (arrayOfMutators, state) => {
   const errorInFirstParam = isFirstParamArray(arrayOfMutators)
   if (errorInFirstParam) throw errorInFirstParam
   return isImmutable(state)
@@ -9,7 +9,7 @@ export const pipe = (arrayOfMutators, state) => {
     : reduceState(arrayOfMutators, state)
 }
 
-export const branchIf = (predicate, runIfTrue, runIfFalse) => state => {
+const branchIf = (predicate, runIfTrue, runIfFalse) => state => {
   const paramsToCheck = runIfFalse
     ? [predicate, runIfTrue, runIfFalse]
     : [predicate, runIfTrue]
@@ -19,10 +19,10 @@ export const branchIf = (predicate, runIfTrue, runIfFalse) => state => {
   return predicate(state) ? runIfTrue(state) : state
 }
 
-export const selector = (reducer, action) =>
+const selector = (reducer, action) =>
   reducer[action.type] ? reducer[action.type](action) : reducer.DEFAULT()
 
-export const selectorPipe = (reducer, state, action, globalMutators = []) => {
+const selectorPipe = (reducer, state, action, globalMutators = []) => {
   return reducer[action.type]
     ? pipe([...reducer[action.type](action), ...globalMutators], state)
     : reducer.DEFAULT()
@@ -32,3 +32,10 @@ const createGroupMutator = (arrayOfMutators, action) => state =>
   arrayOfMutators
     .map(mutator => mutator(action))
     .reduce((state, mutator) => mutator(state), state)
+
+module.exports = {
+  pipe,
+  branchIf,
+  selector,
+  selectorPipe
+}
